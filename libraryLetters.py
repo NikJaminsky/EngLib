7dfb332fc43114be6aca575341b2e1f3273d1150
NikJaminsky <30468788+NikJaminsky@users.noreply.github.com>
2018-02-08 21:50:20 +0800

    Update libraryLetters.py

--- libraryLetters.py
+++ libraryLetters.py
@@ -1,7 +1,7 @@
 import xlrd
 
 
-readFile = xlrd.open_workbook("C:/Users/Nikita/Desktop/engl.xlsx")          #path to library
+readFile = xlrd.open_workbook("C:/Users/"userName"/Desktop/engl.xlsx")          #path to library
 sh = readFile.sheet_by_index(0)                                             #read list of book
 libr = {sh.cell_value(a,3): sh.row_values(a, 4, 8) for a in range(9, 300)}  #filling dict from library
 
